75ab65cff588b7d045f5392943ec35311131cce0
PaulHendi <paulhendi25@gmail.com>
2023-02-26 20:55:33 +0100

    Updated component by adding a tx receipt, and modifying style

--- Frontend/src/components/MintNFT/MintNFTTx.tsx
+++ Frontend/src/components/MintNFT/MintNFTTx.tsx
@@ -2,15 +2,19 @@ import { Contract } from '@ethersproject/contracts'
 import { utils } from 'ethers'
 import { useCall, useLogs, useTokenBalance, useContractFunction } from '@usedapp/core'
 import SFT from '../../abi/SFT.json'
-import { MintNFTContainer } from '../../styles/MintNFT.style'
+import { MintNFTContainer, Input, InputRow, SmallButton } from '../../styles/MintNFT.style'
 import { StatusAnimation } from '../TransactionAnimation'
-import { useEthers } from '@usedapp/core'
+import { useEthers, useTransactions } from '@usedapp/core'
+
 
 export default function MintNFT() {
+    const { transactions } = useTransactions()
+
     const MintNFTContractAddress = "0x6643fBC0D66fc580de15a0A0678D4c1f41b0071b" 
     const MintNFTInterface = new utils.Interface(SFT.abi)
     const contract = MintNFTContractAddress && (new Contract(MintNFTContractAddress, MintNFTInterface) )
     const { state, send } = useContractFunction(contract, 'mint', { transactionName: 'mint' })
+    const {account} = useEthers();
     const MINT_PRICE = 0.01;
 
 
@@ -22,15 +26,34 @@ export default function MintNFT() {
       void send(amount, {value: utils.parseEther(price), gasLimit: 2500000})
     }
 
+    const GetTx = () => {
+
+      let tx = transactions[0].receipt?.transactionHash;
+      let tx_link = "https://testnet.ftmscan.com/tx/" + tx;
+
+      // Get first six and last four characters of the hash
+      let tx_hash = tx?.slice(0, 6) + "..." + tx?.slice(-4);
+
+      return (  <a href={tx_link} target="_blank">{tx_hash}</a> )
+       
+    }
+
 
     return (
       <MintNFTContainer>
         <img src="https://gateway.pinata.cloud/ipfs/QmSe8mwsyJ17QXYn1ka2Tw4z4RSkthoVLJQpvQsoTLgxLD" alt="Obake" />
         <h1>Mint Obake</h1>
-        <p>Cost: 0.01 FTM per NFT</p>
-        <input type="number" placeholder="How many NFT?" className='NFT_number' />
-        <button onClick={() => mint()}>Mint</button>
+        <p>Cost: 0.01 FTM per NFT (Max 5/transaction)</p>
+        <InputRow>
+          <Input type="number" placeholder="How many NFT?" className='NFT_number' />
+          <SmallButton onClick={() => mint()} disabled={!account}>Mint</SmallButton>
+        </InputRow>
         <StatusAnimation transaction={state} />
+      {state.status === 'Success' && transactions.length !== 0 && <GetTx/>}
+
+               
+                
+             
       </MintNFTContainer>
 
     )
@@ -38,3 +61,4 @@ export default function MintNFT() {
 
 
 
+// <td>{transaction.receipt?.blockHash ?? 'Pending...'}</td>
