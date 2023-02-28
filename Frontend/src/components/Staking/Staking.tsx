6a37fd948d2f9b4be6ad9b1a05f50f68b344043f
PaulHendi <paulhendi25@gmail.com>
2023-02-28 21:08:36 +0100

    Added unstake, claim rewards and cleaned a bit

--- Frontend/src/components/Staking/Staking.tsx
+++ Frontend/src/components/Staking/Staking.tsx
@@ -1,41 +1,81 @@
-import React from "react";
-import {StakingContainer, Input, InputRow, SmallButton} from "../../styles/Staking.styles";
+import { useState, useEffect } from "react";
+import {StakingContainer, Input, InputRow, SmallButton, UserRewardContainer} from "../../styles/Staking.styles";
 import { Contract } from '@ethersproject/contracts'
-import { utils } from 'ethers'
-import { useCall, useLogs, useTokenBalance, useContractFunction, useEthers } from '@usedapp/core'
+import { utils, ethers } from 'ethers'
+import { useCall, useContractFunction, useEthers, useTransactions } from '@usedapp/core'
 import Staking from '../../abi/Staking.json'
-import {WalletIcon, SpinnerIcon, CheckIcon, ExclamationIcon} from "../../assets/Icons";
 import { StatusAnimation } from "../TransactionAnimation";
-import { useState } from "react";
+import { STAKING_ADDRESS } from "../../env";
+
+
+const formatter = new Intl.NumberFormat('en-us', {
+    minimumFractionDigits: 4,
+    maximumFractionDigits: 4,
+  })
 
 export default function(){
-    const StakingContractAddress = "0xC26d81929ABC1E74bF39bcA1D0EC7001628e273E"
     const StakingInterface = new utils.Interface(Staking.abi)
-    const contract = StakingContractAddress && (new Contract(StakingContractAddress, StakingInterface) )
-    const { state, send } = useContractFunction(contract, 'stake', { transactionName: 'stake' })
+    const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/fantom_testnet")
+    const contract = new Contract(STAKING_ADDRESS, StakingInterface, provider) 
+
+    const { state : state_stake, send : stakeTx } = useContractFunction(contract, 'stake', { transactionName: 'stake' })
+    const { state : state_unstake, send : unstakeTx } = useContractFunction(contract, 'unstake', { transactionName: 'unstake' })
+    const { state : state_claimReward, send : claimRewardTx } = useContractFunction(contract, 'claimRewards', { transactionName: 'claimRewards' });
+
     const { account } = useEthers()
+    const { transactions } = useTransactions()
     const [disabled, setDisabled] = useState(false)
+    const [isStaking, setIsStaking] = useState(false)
+
+    useEffect(() => {
+        contract.balanceOf(account).then((res:BigInt) => {
+        if (parseInt(res.toString())> 0) setIsStaking(true)});
+    }, [account])
+
 
     const stake = () => {
-
-
-
         setDisabled(true);
         const NFT_to_stake = document.getElementsByName('NFT_to_stake')[0] as HTMLInputElement
-        void send(NFT_to_stake.value)
-
+        void stakeTx(NFT_to_stake.value)
     }
 
+    const unstake = () => {
+        setDisabled(true);
+        const NFT_to_unstake = document.getElementsByName('NFT_to_unstake')[0] as HTMLInputElement
+        void unstakeTx(NFT_to_unstake.value)
+    }
 
 
     const UserReward = () => {
-        const user_reward = useCall({contract : contract, method : 'getRewards', args : [account]})
+
+        const user_reward_reponse = useCall({contract : contract, method : 'getRewards', args : [account]})
+        const user_reward : string = user_reward_reponse?.value?.toString();
         return ( 
-            <div>
-                <p>Current user reward is : </p>
-                <p>{user_reward?.value?.toString() && utils.formatEther(user_reward?.value?.toString())} FTM</p>
-            </div>)
+            <UserRewardContainer>
+                <p>Current user reward : </p>
+                <p>{user_reward && formatter.format(utils.formatEther(user_reward))} FTM</p>
+            </UserRewardContainer>)
     }
+
+    const claimReward = () => {
+        setDisabled(true);
+        void claimRewardTx();
+    }
+
+
+    const GetTx = () => {
+
+        setDisabled(false);
+        let tx = transactions[0].receipt?.transactionHash;
+        let tx_link = "https://testnet.ftmscan.com/tx/" + tx;
+  
+        // Get first six and last four characters of the hash
+        let tx_hash = tx?.slice(0, 6) + "..." + tx?.slice(-4);
+  
+        return (  <a href={tx_link} target="_blank">{tx_hash}</a> )
+         
+      }
+
 
     return(
         <StakingContainer>
@@ -45,9 +85,30 @@ export default function(){
                 <Input type="number" placeholder="amount" name="NFT_to_stake"/>
                 <SmallButton onClick={() => stake()} disabled={!account || disabled}>Stake</SmallButton>
             </InputRow>
-            {account && <UserReward />}
+
+            {account && isStaking  && <UserReward />}
+            {account && isStaking &&  
+                
+                    <SmallButton onClick={() => claimReward()} disabled={!account || disabled}>ClaimReward</SmallButton>
+                 }   
+
+            {account && isStaking && (<p>You can unstake your NFTs here</p>)}
+            {account && isStaking &&  
             
-            <StatusAnimation transaction={state} />
+            <InputRow>
+                
+                <Input type="number" placeholder="amount" name="NFT_to_unstake"/>
+                <SmallButton onClick={() => unstake()} disabled={!account || disabled}>Unstake</SmallButton>
+            </InputRow> }   
+
+            {state_stake.status !== 'None' && <StatusAnimation transaction={(state_stake)}/>}
+            {state_unstake.status !== 'None'  && <StatusAnimation transaction={(state_unstake)}/>}
+            {state_claimReward.status !== 'None'  && <StatusAnimation transaction={(state_claimReward)}/>}
+            
+            {(state_stake.status === 'Success' || 
+             state_unstake.status === 'Success' ||
+             state_claimReward.status === 'Success') && transactions.length !== 0 && <GetTx/>}
+
         </StakingContainer>
     );
 }
